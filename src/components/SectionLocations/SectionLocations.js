65a323cc38f0d354a2040d7fc976002304856f01
Mikko Koski <mikkokos@gmail.com>
2020-04-08 21:45:39 +0300

    Initial customizations

--- src/components/SectionLocations/SectionLocations.js
+++ src/components/SectionLocations/SectionLocations.js
@@ -8,9 +8,9 @@ import { NamedLink } from '../../components';
 
 import css from './SectionLocations.css';
 
-import helsinkiImage from './images/location_helsinki.jpg';
-import rovaniemiImage from './images/location_rovaniemi.jpg';
-import rukaImage from './images/location_ruka.jpg';
+import ruohonleikkuritImage from './images/ruohonleikkurit.jpg';
+import trimmeritImage from './images/trimmerit.jpg';
+import oksasilppuritImage from './images/oksasilppurit.jpg';
 
 class LocationImage extends Component {
   render() {
@@ -29,11 +29,8 @@ const locationLink = (name, image, searchQuery) => {
           <LazyImage src={image} alt={name} className={css.locationImage} />
         </div>
       </div>
-      <div className={css.linkText}>
-        <FormattedMessage
-          id="SectionLocations.listingsInLocation"
-          values={{ location: nameText }}
-        />
+    <div className={css.linkText}>
+      {nameText}
       </div>
     </NamedLink>
   );
@@ -51,19 +48,19 @@ const SectionLocations = props => {
       </div>
       <div className={css.locations}>
         {locationLink(
-          'Helsinki',
-          helsinkiImage,
-          '?address=Helsinki%2C%20Finland&bounds=60.2978389%2C25.254484899999966%2C59.9224887%2C24.782875800000056&origin=60.16985569999999%2C24.93837910000002'
+          'Ruohonleikkurit',
+          ruohonleikkuritImage,
+          '?category=ruohonleikkurit'
         )}
         {locationLink(
-          'Rovaniemi',
-          rovaniemiImage,
-          '?address=Rovaniemi%2C%20Finland&bounds=67.18452510000002%2C27.32667850000007%2C66.1553745%2C24.736871199999996&origin=66.50394779999999%2C25.729390599999988'
+          'Trimmerit',
+          trimmeritImage,
+          '?category=trimmerit'
         )}
         {locationLink(
-          'Ruka',
-          rukaImage,
-          '?address=Ruka%2C%20Finland&bounds=66.1704578%2C29.14246849999995%2C66.1614402%2C29.110453699999994&origin=66.16594940000002%2C29.12646110000003'
+          'Oksasilppurit',
+          oksasilppuritImage,
+          '?category=oksasilppurit'
         )}
       </div>
     </div>
